97adcb62f9ddb4a569fcd4a4b6ef18640b6c9463
veil <auphong2707@gmail.com>
2025-03-26 21:13:06 +0700

    Add temp files

---
+++ models/efficientnet_b0.py
@@ -0,0 +1,2 @@
+def get_efficientnet_b0(num_classes, pretrained=True, freeze=False):
+    raise NotImplementedError("EfficientNet-B0 model not implemented yet")
---
+++ models/vgg16.py
@@ -0,0 +1,2 @@
+def get_vgg16(num_classes, pretrained=True, freeze=False):
+    raise NotImplementedError("VGG16 model not implemented yet")
