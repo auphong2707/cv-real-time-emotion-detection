57b5354f1f27c4e926a66c8f0419a24e3eabe433
veil <auphong2707@gmail.com>
2025-05-09 23:22:22 +0700

    Update emotion_cnn.py

--- models/emotion_cnn.py
+++ models/emotion_cnn.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Squeeze-and-Excitation Block
+# Squeeze-and-Excitation Block (EfficientNet/MobileNetV3)
 class SEBlock(nn.Module):
     def __init__(self, in_channels, reduction=16):
         super(SEBlock, self).__init__()
@@ -18,9 +18,9 @@ class SEBlock(nn.Module):
         y = self.sigmoid(self.fc2(y)).view(b, c, 1, 1)
         return x * y
 
-# Mobile Inverted Bottleneck Convolution (MBConv)
+# Mobile Inverted Bottleneck Convolution (EfficientNet)
 class MBConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, expand_ratio=4, kernel_size=3, stride=1):
+    def __init__(self, in_channels, out_channels, expand_ratio=6, kernel_size=3, stride=1):
         super(MBConvBlock, self).__init__()
         hidden_dim = in_channels * expand_ratio
         self.stride = stride
@@ -37,7 +37,7 @@ class MBConvBlock(nn.Module):
     def forward(self, x):
         identity = x
         out = self.expand(x)
-        out = F.silu(self.bn0(out))
+        out = F.silu(self.bn0(out))  # Swish activation
         out = self.depthwise(out)
         out = F.silu(self.bn1(out))
         out = self.se(out)
@@ -47,7 +47,7 @@ class MBConvBlock(nn.Module):
             out = out + identity
         return out
 
-# Depthwise Separable Convolution
+# Depthwise Separable Convolution (MobileNetV3)
 class DepthwiseSeparableConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1):
         super(DepthwiseSeparableConv, self).__init__()
@@ -59,66 +59,82 @@ class DepthwiseSeparableConv(nn.Module):
 
     def forward(self, x):
         out = self.depthwise(x)
-        out = F.hardswish(self.bn1(out))
+        out = F.hardswish(self.bn1(out))  # h-swish activation
         out = self.pointwise(out)
         out = self.bn2(out)
         out = self.se(out)
         return out
 
-# Efficient EmotionCNN
+# Complex EmotionCNN
 class EmotionCNN(nn.Module):
     def __init__(self, num_classes=7, in_channels=3, image_size=224):
         super(EmotionCNN, self).__init__()
         self.image_size = image_size
 
-        # Block 1: VGG-style (reduced channels)
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        # Block 1: VGG-style (stacked 3x3 convs)
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  # image_size -> image_size/2
-        self.dropout1 = nn.Dropout(0.2)
+        self.dropout1 = nn.Dropout(0.1)
 
-        # Block 2: MBConv
-        self.mbconv1 = MBConvBlock(64, 96, expand_ratio=4, kernel_size=3, stride=2)  # image_size/2 -> image_size/4
-        self.dropout2 = nn.Dropout(0.2)
+        # Block 2: MBConv (EfficientNet-inspired)
+        self.mbconv1 = MBConvBlock(128, 256, expand_ratio=6, kernel_size=3, stride=2)  # image_size/2 -> image_size/4
+        self.dropout2 = nn.Dropout(0.1)
 
-        # Block 3: Depthwise Separable Conv
-        self.dsconv1 = DepthwiseSeparableConv(96, 192, kernel_size=3, stride=2)  # image_size/4 -> image_size/8
-        self.dropout3 = nn.Dropout(0.2)
+        # Block 3: MBConv (additional for complexity)
+        self.mbconv2 = MBConvBlock(256, 256, expand_ratio=6, kernel_size=3, stride=1)
+        self.dropout3 = nn.Dropout(0.1)
 
-        # Block 4: Depthwise Separable Conv (replacing VGG-style)
-        self.dsconv2 = DepthwiseSeparableConv(192, 192, kernel_size=3, stride=2)  # image_size/8 -> image_size/16
-        self.dropout4 = nn.Dropout(0.2)
+        # Block 4: Depthwise Separable Conv (MobileNetV3-inspired)
+        self.dsconv1 = DepthwiseSeparableConv(256, 512, kernel_size=3, stride=2)  # image_size/4 -> image_size/8
+        self.dropout4 = nn.Dropout(0.1)
 
-        # Global Average Pooling
+        # Block 5: Depthwise Separable Conv (additional for complexity)
+        self.dsconv2 = DepthwiseSeparableConv(512, 512, kernel_size=3, stride=2)  # image_size/8 -> image_size/16
+        self.dropout5 = nn.Dropout(0.1)
+
+        # Global Average Pooling and FC layers
         self.global_pool = nn.AdaptiveAvgPool2d(1)
         self.flatten = nn.Flatten()
-        self.fc = nn.Linear(192, num_classes)
+        self.fc1 = nn.Linear(512, 512)  # Intermediate FC layer
+        self.bn_fc1 = nn.BatchNorm1d(512)
+        self.dropout6 = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(512, num_classes)
 
     def forward(self, x):
-        # Block 1
+        # Block 1: VGG-style
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
         x = self.pool1(x)
         x = self.dropout1(x)
 
-        # Block 2
+        # Block 2: MBConv
         x = self.mbconv1(x)
         x = self.dropout2(x)
 
-        # Block 3
-        x = self.dsconv1(x)
+        # Block 3: MBConv
+        x = self.mbconv2(x)
         x = self.dropout3(x)
 
-        # Block 4
-        x = self.dsconv2(x)
+        # Block 4: Depthwise Separable
+        x = self.dsconv1(x)
         x = self.dropout4(x)
 
-        # Global pooling and classification
+        # Block 5: Depthwise Separable
+        x = self.dsconv2(x)
+        x = self.dropout5(x)
+
+        # Global pooling and FC layers
         x = self.global_pool(x)
         x = self.flatten(x)
-        x = self.fc(x)
+        x = F.relu(self.bn_fc1(self.fc1(x)))
+        x = self.dropout6(x)
+        x = self.fc2(x)
         return x
 
 # Example usage
